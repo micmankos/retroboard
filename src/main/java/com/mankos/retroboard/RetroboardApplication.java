272be7d03ca4b38343d3152efd9a9e7324026581
Michael Mankos <micmankos@gmail.com>
2021-02-07 15:43:17 -0500

    And logging actually works!

--- src/main/java/com/mankos/retroboard/RetroboardApplication.java
+++ src/main/java/com/mankos/retroboard/RetroboardApplication.java
@@ -3,10 +3,18 @@ package com.mankos.retroboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 public class RetroboardApplication {
 
 	public static void main(String[] args) {
+      //Creating the Logger object
+      Logger logger = LoggerFactory.getLogger("SampleLogger");
+
+      //Logging the information
+      logger.info("Hi This is my first SLF4J program");
 		SpringApplication.run(RetroboardApplication.class, args);
 	}
 
